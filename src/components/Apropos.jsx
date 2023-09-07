6b2a604f7e424244e24134da3d696727e63c8ab3
GroupeSgbd <azizprofess@outlook.fr>
2023-09-07 12:23:44 +0000

    seond commit

--- src/components/Apropos.jsx
+++ src/components/Apropos.jsx
@@ -16,9 +16,14 @@ const Apropos = () => {
   const navigate = useNavigate();
   return (
     <>
-   
-      <div id="about" className="relative justify-center items-center overflow-hidden md:px-8 md:mx-auto  w-full  bg-gray-100 md:my-10 my-5  flex flex-col md:flex-row md:gap-5">
-      <ContactPoppup openPoppup={openAboutPoppup} closePoppup={setOpenAboutPoppup}/>
+      <div
+        id="about"
+        className="relative justify-center items-center overflow-hidden md:px-8 md:mx-auto  w-full  bg-gray-100 md:my-10 my-5  flex flex-col md:flex-row md:gap-5"
+      >
+        <ContactPoppup
+          openPoppup={openAboutPoppup}
+          closePoppup={setOpenAboutPoppup}
+        />
         <div className=" md:rounded-none md:h-full mx-auto md:w-[50%]  h-full bg-[#7280FF] w-[70%] py-7 mt-14 rounded-md ">
           <img
             src={img2}
@@ -36,17 +41,28 @@ const Apropos = () => {
             />
           </p>
           <ul className=" flex gap-6 justify-center my-6">
+            <a href="https://github.com/AbdouAziz13">
+              {" "}
+              <li className="text-white hover:scale-y-90 duration-300 cursor-pointer">
+                <AiOutlineGithub size={27} />
+              </li>
+            </a>
+
+            <a href="https://www.instagram.com/lokoazz23/?hl=fr">
+              {" "}
+              <li className="text-white hover:scale-y-90 duration-300 cursor-pointer">
+                <AiFillInstagram size={27} />
+              </li>
+            </a>
+
             <li className="text-white hover:scale-y-90 duration-300 cursor-pointer">
-              <AiOutlineGithub size={27} />
+              <AiOutlineWhatsApp
+                size={27}
+                onClick={() => setOpenAboutPoppup(true)}
+              />
             </li>
             <li className="text-white hover:scale-y-90 duration-300 cursor-pointer">
-              <AiFillInstagram size={27} />
-            </li>
-            <li className="text-white hover:scale-y-90 duration-300 cursor-pointer">
-              <AiOutlineWhatsApp size={27} onClick={()=> setOpenAboutPoppup(true)} />
-            </li>
-            <li className="text-white hover:scale-y-90 duration-300 cursor-pointer">
-              <AiFillPhone size={27}  onClick={()=> setOpenAboutPoppup(true)}/>
+              <AiFillPhone size={27} onClick={() => setOpenAboutPoppup(true)} />
             </li>
           </ul>
         </div>
